7141684e5a624f91448b4509d28c26a01888ee71
Jan Willems <jw@elevenbits.com>
2024-11-10 21:49:19 +0100

    :sparkles: Very basic game!

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,68 @@
-use std::io::stdin;
+use bracket_lib::prelude::*;
 
-#[derive(Debug)]
-enum VisitorAction {
-    Accept,
-    AcceptWithNote { note: String },
-    Refuse,
-    Probation,
+struct State {
+    mode: GameMode,
 }
 
-#[derive(Debug)]
-struct Visitor {
-    name: String,
-    action: VisitorAction,
-    age: i8,
-}
-
-impl Visitor {
-    fn new(name: &str, action: VisitorAction, age: i8) -> Self {
-        Self {
-            name: name.to_lowercase(),
-            action,
-            age,
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
         }
     }
-    fn greet_visitor(&self) {
-        match &self.action {
-            VisitorAction::Accept => println!("Welcome to the tree house, {}", self.name),
-            VisitorAction::AcceptWithNote { note } => {
-                println!("Welcome to the treehouse, {}", self.name);
-                println!("{}", note);
-                if self.age < 21 {
-                    println!("Do not serve alcohol to {}", self.name);
-                }
-            }
-            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
-            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
-        }
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
     }
-
-}
-
-fn what_is_your_name() -> String {
-    let mut your_name = String::new();
-    stdin()
-        .read_line(&mut your_name)
-        .expect("Failed to read line");
-    your_name.trim().to_lowercase()
-}
-
-fn main() {
-    let mut visitor_list = vec![
-        Visitor::new("bert", VisitorAction::Accept, 45),
-        Visitor::new("steve",VisitorAction::AcceptWithNote {
-            note: String::from("Lactose-free milk is in the fridge")
-        }, 15),
-        Visitor::new("fred", VisitorAction::Refuse, 30),
-    ];
-    loop {
-        println!("Hello, what is your name? (Leave empty and press ENTER to quit)");
-        let name = what_is_your_name();
-        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
-        match known_visitor {
-            Some(visitor) => visitor.greet_visitor(),
-            None => {
-                if name.is_empty() {
-                    break;
-                } else {
-                    println!("{} is not on the visitors list.", name);
-                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
-                }
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game)");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
             }
         }
     }
-    println!("The final list of visitors:");
-    println!("{:#?}", visitor_list);
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+    main_loop(context, State::new())
 }
