ce1a59f23eaedfe9ee9a596f409d849c7a43b5cb
Jan Willems <jw@elevenbits.com>
2024-11-15 22:26:08 +0100

    :sparkles: We all take turns!

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -5,18 +5,16 @@ use crate::prelude::*;
 #[read_component(MovingRandomly)]
 pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
     let mut movers = <(&mut Point, &MovingRandomly)>::query();
-    movers
-        .iter_mut(ecs)
-        .for_each(|(pos, _)| {
-            let mut rng = RandomNumberGenerator::new();
-            let destination = match rng.range(0, 4) {
-                0 => Point::new(-1, 0),
-                1 => Point::new(1, 0),
-                2 => Point::new(0, -1),
-                _ => Point::new(0, 1),
-            } + *pos;
-            if map.can_enter_tile(destination) {
-                *pos = destination;
-            }
-        })
+    movers.iter_mut(ecs).for_each(|(pos, _)| {
+        let mut rng = RandomNumberGenerator::new();
+        let destination = match rng.range(0, 4) {
+            0 => Point::new(-1, 0),
+            1 => Point::new(1, 0),
+            2 => Point::new(0, -1),
+            _ => Point::new(0, 1),
+        } + *pos;
+        if map.can_enter_tile(destination) {
+            *pos = destination;
+        }
+    })
 }
