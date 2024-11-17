9e2ffde3659ba862fbf76c3bc8758d8245d236fb
Jan Willems <jw@elevenbits.com>
2024-11-17 19:12:26 +0100

    :recycle: Movements are handled via Legion Commands.

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -6,6 +6,7 @@ mod entity_render;
 mod map_render;
 mod player_input;
 mod random_move;
+mod movement;
 
 pub fn build_input_scheduler() -> Schedule {
     Schedule::builder()
@@ -18,6 +19,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
@@ -30,21 +33,10 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(end_turn::end_turn_system())
-        .build()
-}
-
-pub fn build_scheduler() -> Schedule {
-    Schedule::builder()
-        .add_system(player_input::player_input_system())
-        .add_system(collisions::collisions_system())
-        .flush()
-        .add_system(map_render::map_render_system())
-        .add_system(entity_render::entity_render_system())
-        .add_system(random_move::random_move_system())
         .build()
 }
