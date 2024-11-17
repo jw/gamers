b75fc0815c4ce57a40987fa31e9c72488fd43b1d
Jan Willems <jw@elevenbits.com>
2024-11-17 21:35:13 +0100

    :sparkles: Added HUD!

--- src/spawner.rs
+++ src/spawner.rs
@@ -8,10 +8,18 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
         },
+        Health {
+            current: 20,
+            max: 20,
+        }
     ));
 }
 
 pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    let (hp, name, _) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc(),
+    };
     ecs.push((
         Enemy,
         pos,
@@ -25,5 +33,15 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
             },
         },
         MovingRandomly {},
+        Health {current: hp, max: hp},
+        Name(name),
     ));
+}
+
+fn goblin() -> (i32, String, FontCharType) {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
 }
